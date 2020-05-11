18d153fb342776e35e68246a076b0b6c8a86e57d
wkl007 <499657357@qq.com>
2020-05-11 19:07:38 +0800

    feat: 全局导入插件完成

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import WklUI from '../components/index'
-import '../components/style'
+
+/* 按需引入 */
+import WklUI from 'wkl-ui/lib/wkl-ui.umd'
 
 Vue.use(WklUI)
 
