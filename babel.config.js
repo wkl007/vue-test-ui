b7ca7ca7ee97569ae67f0634000e938102e897dc
wkl007 <499657357@qq.com>
2020-05-11 17:14:05 +0800

    feat: 项目配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    ['import',
+      {
+        libraryName: 'wkl-ui',
+        libraryDirectory: 'components',
+        style: true
+      }
+    ]
   ]
 }
